6788f847f2c51ef86c7b1057801c0b704765ae9b
hyunkark <hyunkark@gmail.com>
2022-02-28 16:49:57 +0900

    Add: Add signup page view - Fixed Layout by using Flexbox - Fixed signup page  by reusing components - Fix signup.tsx to manage signup input values in signup.tsx

--- src/screens/Entry.tsx
+++ src/screens/Entry.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import styled from 'styled-components/native';
 
 interface Props {
   navigation: StackNavigationProp<AuthStackParamList, 'name'>;
@@ -11,15 +12,21 @@ const Entry = ({navigation}: Props) => {
     navigation.navigate(name);
   };
   return (
-    <View>
+    <EntryView>
       <TouchableOpacity onPress={() => changeScreen('SignUp')}>
         <Text>회원가입</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => changeScreen('SignIn')}>
         <Text>로그인</Text>
       </TouchableOpacity>
-    </View>
+    </EntryView>
   );
 };
+
+const EntryView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Entry;
---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import {DefaultTheme} from 'styled-components/native';
+
+const theme: DefaultTheme = {
+  color: {
+    primary: '#065E85',
+    secondary: '#004D73',
+    white: '#FFFFFF',
+    black: '#000000',
+    'gray-100': '#C4C4C4',
+    'gray-200': '#3E3E3E',
+    'gray-300': '#777777',
+    'red-100': '#E01E1E',
+    'red-200': '#D60F0F',
+  },
+  fontSize: {
+    heading1: '18px',
+    body: '12px',
+  },
+};
+
+export default theme;
