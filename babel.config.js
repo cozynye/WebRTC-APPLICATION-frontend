892b592c8a8a7794c71d652c675a92cc0ff07abc
hyunkark <hyunkark@gmail.com>
2022-02-28 11:03:24 +0900

    Add: theme.js에 폰트 크기를 설정하고, 절대경로를 설정함

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@query': './src/query',
+        },
+      },
+    ],
+  ],
 };
