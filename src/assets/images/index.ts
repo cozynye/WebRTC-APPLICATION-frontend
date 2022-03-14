faede79cddd7ba7903c160ce5a5b5c14acb27856
cozynye <cozynye@gmail.com>
2022-03-14 13:00:57 +0900

    Fix : PR#5 comment resolve

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import CameraImage from './camera_revers_btn.png';
+import AudioOnImage from './audio_on_btn.png';
+import VideoOnImage from './video_on_btn.png';
+import VideoOffImage from './video_off_btn.png';
+
+export const imageFile = {
+  CameraImage,
+  AudioOnImage,
+  VideoOnImage,
+  VideoOffImage,
+};
